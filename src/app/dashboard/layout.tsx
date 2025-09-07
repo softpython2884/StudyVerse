f3ccb8cd8f0f144b2104eb0da787ad101816d5b5
Enzo prados <enzo.prados@gmail.com>
2025-09-07 08:58:53 +0000

    Try fixing this error: `Runtime Error: TypeError: Cannot read properties

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,13 +3,14 @@
 import * as React from 'react';
 import { DashboardPage } from '@/components/dashboard-page';
 import { getCurrentUser } from '@/lib/user';
+import { protectedRoute } from '@/lib/session';
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode,
 }) {
-  const user = await getCurrentUser();
+  const user = await protectedRoute();
 
   return (
     <DashboardPage user={user}>
