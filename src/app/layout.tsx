d3e2584ef13ae4ecab18a11caa3167cd8f21a5d5
Enzo prados <enzo.prados@gmail.com>
2025-09-12 13:27:47 +0000

    ah, oe nan je pense pas utiliser firebase storage ou quoi, j'ai pas vrai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from 'next';
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 import { cn } from '@/lib/utils';
+import { PwaLoader } from '@/components/pwa-loader';
 
 export const metadata: Metadata = {
   title: 'StudyVerse - AI-Powered Learning Environment',
   description: 'Create, organize, and enhance your notes with the power of AI. StudyVerse offers real-time speech-to-text, note refinement, diagram generation, and more.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -19,8 +21,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;500;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#8b9fb1" />
       </head>
       <body className={cn('font-body antialiased')}>
+        <PwaLoader />
         {children}
         <Toaster />
       </body>
